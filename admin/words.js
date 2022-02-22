5f41578ec4eae687fa6a64290521e8f5cceda648
necotec <o.alff@mailbox.org>
2022-02-22 19:06:55 +0100

    Initial commit.

---
+++ admin/words.js
@@ -0,0 +1,41 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+	'signal-messenger adapter settings': {
+		'en': 'Adapter settings for signal-messenger',
+		'de': 'Adaptereinstellungen für signal-messenger',
+		'ru': 'Настройки адаптера для signal-messenger',
+		'pt': 'Configurações do adaptador para signal-messenger',
+		'nl': 'Adapterinstellingen voor signal-messenger',
+		'fr': "Paramètres d'adaptateur pour signal-messenger",
+		'it': "Impostazioni dell'adattatore per signal-messenger",
+		'es': 'Ajustes del adaptador para signal-messenger',
+		'pl': 'Ustawienia adaptera dla signal-messenger',
+		'zh-cn': 'signal-messenger的适配器设置'
+	},
+	'option1': {
+		'en': 'option1',
+		'de': 'Option 1',
+		'ru': 'Опция 1',
+		'pt': 'Opção 1',
+		'nl': 'Optie 1',
+		'fr': 'Option 1',
+		'it': 'opzione 1',
+		'es': 'Opción 1',
+		'pl': 'opcja 1',
+		'zh-cn': '选项1'
+	},
+	'option2': {
+		'en': 'option2',
+		'de': 'Option 2',
+		'ru': 'вариант 2',
+		'pt': 'opção 2',
+		'nl': 'Optie 2',
+		'fr': 'Option 2',
+		'it': 'opzione 2',
+		'es': 'opcion 2',
+		'pl': 'Opcja 2',
+		'zh-cn': '选项2'
+	}
+};
